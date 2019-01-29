86aee61ab6bb2fbfeab67bca9d1443f85b0744b2
Sk1Ev <skoroxod123@gmail.com>
2019-01-29 17:47:45 +0300

    Hello

---
+++ ll.php
@@ -0,0 +1,9 @@
+<?php echo "Hello,world!"
+ echo "My name is Jony,"
+ echo "I live is Magnitigorsk!"
+ echo "Good bay!"
+ echo "Привет!"
+ echo "Меня зовут Женя!"
+ echo "Я живу в Магнитогорске!";?>
+
+
